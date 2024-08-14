12d23e5f8ffc177bb420d3ffebc7e79f0eb1d167
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-15 01:08:44 +0530

    customer side

--- src/main/java/com/example/posbackendjakartaee/Controller/Customer.java
+++ src/main/java/com/example/posbackendjakartaee/Controller/Customer.java
@@ -1,4 +1,53 @@
 package com.example.posbackendjakartaee.Controller;
 
-public class Customer {
+import com.example.posbackendjakartaee.DTO.CustomerDTO;
+import com.example.posbackendjakartaee.Dao.CustomerDao;
+import com.example.posbackendjakartaee.Dao.IMPL.CustomerDAOIMPL;
+import jakarta.json.bind.Jsonb;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.json.bind.JsonbBuilder;
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = ("/customer"))
+public class Customer extends HttpServlet {
+    Connection connection;
+    public void init() throws ServletException {
+        try {
+            var driver = getServletContext().getInitParameter("driver-class");
+            var dburl = getServletContext().getInitParameter("dbURL");
+            var username = getServletContext().getInitParameter("dbUserName");
+            var password = getServletContext().getInitParameter("dbPassword");
+
+            Class.forName(driver);
+            this.connection = DriverManager.getConnection(dburl, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(!req.getContentType().toLowerCase().startsWith("application/json")|| req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+            try (Writer writer =resp.getWriter()){
+                var jsonb = JsonbBuilder.create();
+                CustomerDTO customerDTO =jsonb.fromJson(req.getReader(), CustomerDTO.class);
+                var customerDAOIMPL = new CustomerDAOIMPL();
+                System.out.println(customerDTO);
+                if (customerDAOIMPL.saveCustomer(customerDTO,connection)){
+                    writer.write("customer saved successfully");
+                }else {
+                    writer.write("something went wrong");
+                }
+            }
+    }
 }
--- src/main/java/com/example/posbackendjakartaee/Dao/IMPL/CustomerDAOIMPL.java
+++ src/main/java/com/example/posbackendjakartaee/Dao/IMPL/CustomerDAOIMPL.java
@@ -1,4 +1,41 @@
 package com.example.posbackendjakartaee.Dao.IMPL;
 
-public class CustomerDAOIMPL {
+import com.example.posbackendjakartaee.DTO.CustomerDTO;
+import com.example.posbackendjakartaee.Dao.CustomerDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CustomerDAOIMPL implements CustomerDao {
+        static String save_customer="INSERT INTO customer VALUES (?,?,?,?)";
+
+        @Override
+        public CustomerDTO getCustomer(String CID, Connection connection) {
+                return null;
+        }
+
+        public boolean saveCustomer(CustomerDTO customerDTO, Connection connection){
+                try {
+                        PreparedStatement preparedStatement=connection.prepareStatement(save_customer);
+                        preparedStatement.setString(1,customerDTO.getId());
+                        preparedStatement.setString(2,customerDTO.getName());
+                        preparedStatement.setString(3,customerDTO.getAddress());
+                        preparedStatement.setString(4,customerDTO.getMobile());
+                                return preparedStatement.executeUpdate()!=0;
+                } catch (SQLException e) {
+                        e.printStackTrace();
+                }
+                return false;
+        }
+
+        @Override
+        public boolean deleteCustomer(String CID, Connection connection) {
+                return false;
+        }
+
+        @Override
+        public boolean updateCustomer(String CID, CustomerDTO customerDTO, Connection connection) {
+                return false;
+        }
 }
