be0a07f1b92fa7e358d3e5e8c97f2d35a9bae31c
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-15 23:29:53 +0530

    created item files and have to do get method

--- src/main/java/com/example/posbackendjakartaee/Controller/ItemController.java
+++ src/main/java/com/example/posbackendjakartaee/Controller/ItemController.java
@@ -1,5 +1,9 @@
 package com.example.posbackendjakartaee.Controller;
 
+import com.example.posbackendjakartaee.DTO.ItemDTO;
+import com.example.posbackendjakartaee.Dao.IMPL.ItemDAOIMPL;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,10 +11,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.Writer;
+import java.sql.Connection;
 
 
 @WebServlet(urlPatterns = "/item")
 public class ItemController extends HttpServlet {
+    Connection connection;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
@@ -18,16 +25,49 @@ public class ItemController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        if(!req.getContentType().toLowerCase().startsWith("application/json")|| req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        try (Writer writer=resp.getWriter()){
+            var jsonb = JsonbBuilder.create();
+            var itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+            var itemDAOIMPL = new ItemDAOIMPL();
+            System.out.println(itemDTO);
+                if (itemDAOIMPL.saveItem(itemDTO,connection)){
+                    writer.write("item saved successfully");
+                }else{
+                    writer.write("something went wrong");
+                }
+        }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        try(Writer writer=resp.getWriter()){
+            var code=req.getParameter("code");
+            var jsonb = JsonbBuilder.create();
+            ItemDAOIMPL itemDAOIMPL = new ItemDAOIMPL();
+            var itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+                if (itemDAOIMPL.updateItem(code,itemDTO,connection)){
+                    writer.write("item updated successfully");
+                }else{
+                    writer.write("something went wrong");
+                }
+
+
+        }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        try(Writer writer=resp.getWriter()){
+            var code=req.getParameter("code");
+            var itemDAOIMPL = new ItemDAOIMPL();
+                if (itemDAOIMPL.deleteItem(code,connection)){
+                    writer.write("item deleted successfully");
+                }else{
+                    writer.write("something went wrong");
+                }
+        }
     }
 }
