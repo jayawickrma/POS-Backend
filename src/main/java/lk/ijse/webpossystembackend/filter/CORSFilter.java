e33bdc881150348b292a7e8ce50484fb1d8f9039
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-27 21:59:51 +0530

    edited something

--- src/main/java/lk/ijse/webpossystembackend/filter/CORSFilter.java
+++ src/main/java/lk/ijse/webpossystembackend/filter/CORSFilter.java
@@ -7,29 +7,24 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @WebFilter(filterName = "CORSFilter", urlPatterns = "/*")
 public class CORSFilter extends HttpFilter {
-    Logger logger = LoggerFactory.getLogger(CORSFilter.class);
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        String origin = req.getHeader("Origin");
-        if (origin.contains(getServletContext().getInitParameter("Origin"))){
+        var origin = req.getHeader("Origin");
+        var configuredOrigin = getServletContext().getInitParameter("origin");
+        System.out.println("Origin: " + origin);
+        System.out.println("Configured Origin: " + configuredOrigin);
+        if(origin!=null && origin.contains(configuredOrigin)){
             res.setHeader("Access-Control-Allow-Origin", origin);
-            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEADER");
-            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers","Content-Type");
             res.setHeader("Access-Control-Expose-Headers", "Content-Type");
-
-            logger.info("Allow access to " + origin + " origin");
-            chain.doFilter(req, res);
-        }else{
-            logger.error("Access denied for " + origin + " origin");
-            res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
+        chain.doFilter(req, res);
     }
 }
